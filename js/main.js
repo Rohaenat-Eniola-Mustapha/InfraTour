4b59b7ceca900bef4d4f7ffc9e8a8e4d0b12c54f
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2024-07-04 15:55:06 +0100

    Updated more and add more

--- js/main.js
+++ js/main.js
@@ -43,3 +43,13 @@ document.addEventListener("DOMContentLoaded", () => {
         }, stepTime);
     }
 });
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
