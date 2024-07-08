cd80ebd1e9936d31edb5fb7af40f09fb8813e388
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2024-07-08 19:55:13 +0100

    Update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root', // use your MySQL username
-    password: '', // use your MySQL password
+    password: 'root', // use your MySQL password
     database: 'infratour'
 });
 
