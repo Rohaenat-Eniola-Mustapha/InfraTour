b8017e291ee7a222204da90fb73c7a9fdff2dc9b
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2024-07-09 19:45:57 +0100

    Update

--- js/register.js
+++ js/register.js
@@ -1,25 +1,13 @@
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+const container = document.getElementById('container');
 
-    const username = document.querySelector('input[name="username"]').value;
-    const email = document.querySelector('input[name="email"]').value;
-    const password = document.querySelector('input[name="password"]').value;
+const registerBtn = document.getElementById('register');
 
-    const data = { username, email, password };
+const loginBtn = document.getElementById('login');
 
-    fetch('http://localhost:3000/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.text())
-    .then(data => {
-        alert(data);
-        // Redirect to login page or clear form
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
 });
