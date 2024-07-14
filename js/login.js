ed7eabadd7adccc95d408b06423b57c7186fefdd
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2024-07-14 06:44:23 +0100

    Update register

--- js/login.js
+++ js/login.js
@@ -9,3 +9,15 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const formType = urlParams.get('form');
+    const container = document.getElementById('container');
+
+    if (formType === 'login') {
+        container.classList.add('active');
+    } else if (formType === 'register') {
+        container.classList.remove('active');
+    }
+});
